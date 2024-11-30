f97838b42c7fc06413b4a87e264c9a4275498268
AliTaleshi <alitys083@gmail.com>
2024-11-30 16:41:40 +0330

    Push base project

---
+++ src/main/java/com/expense/split/SplitExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.expense.split;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expense/split/SplitExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expense.split;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
