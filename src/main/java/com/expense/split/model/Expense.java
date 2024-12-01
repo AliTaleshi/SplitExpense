545d7cccba62b189d88e4b4d1088dd7a8b0263d4
AliTaleshi <alitys083@gmail.com>
2024-12-01 10:56:44 +0330

    Add User Expense relationship

--- src/main/java/com/expense/split/model/Expense.java
+++ src/main/java/com/expense/split/model/Expense.java
@@ -1,5 +1,6 @@
 package com.expense.split.model;
 
+import java.util.List;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -19,4 +20,16 @@ public class Expense {
     private LocalDateTime date;
     @UpdateTimestamp
     private LocalDateTime updateDate;
+
+    @ManyToOne
+    @JoinColumn(name = "payer_id", nullable = false)
+    private User payer;
+
+    @ManyToMany
+    @JoinTable(
+            name = "expense_debtors",
+            joinColumns = @JoinColumn(name = "expense_id"),
+            inverseJoinColumns = @JoinColumn(name = "debtor_id")
+    )
+    private List<User> debtors;
 }
